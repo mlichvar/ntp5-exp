0da442a64e1189158ffef369d0bf88cc46a9ad4f
Miroslav Lichvar <mlichvar@redhat.com>
2023-03-07 15:06:13 +0100

    add reference and secondary rx timestamp extension fields

--- ntpnode.py
+++ ntpnode.py
@@ -62,6 +62,9 @@ class NtpEF(enum.IntEnum):
     REFERENCE_IDS_RESP = 0xf504
     SERVER_INFO = 0xf505
     CORRECTION = 0xf506
+    REFERENCE_TS = 0xf507
+    MONOTONIC_RX_TS = 0xf508
+    SECONDARY_RX_TS = 0xf509
     DRAFT_ID = 0xf5ff
 
 OUR_DRAFT_ID = "draft-mlichvar-ntp-ntpv5-07     "
@@ -105,6 +108,8 @@ class NtpMessage:
     server_info: int = None # bit field
     reference_ids_req: tuple = None # (offset, length)
     reference_ids_resp: bytes = None
+    reference_ts_: int = None
+    secondary_rx_ts: dict = None # timescale -> (era, timestamp)
     draft_id: str = None
 
     @classmethod
@@ -137,7 +142,8 @@ class NtpMessage:
             raise ValueError("Invalid version {}".format(version))
 
         extensions = message[48:]
-        server_info = reference_ids_req = reference_ids_resp = draft_id = None
+        server_info = reference_ids_req = reference_ids_resp = reference_ts_ = None
+        secondary_rx_ts = draft_id = None
 
         while len(extensions) > 0:
             # Ignore NTPv4 MAC
@@ -154,6 +160,14 @@ class NtpMessage:
                 reference_ids_resp = extensions[4:ef_len]
             elif ef_type == NtpEF.SERVER_INFO and ef_len == 8:
                 server_info = struct.unpack("!H", extensions[4:6])[0]
+            elif ef_type == NtpEF.REFERENCE_TS and ef_len == 12:
+                reference_ts_ = struct.unpack("!Q", extensions[4:12])[0]
+            elif ef_type == NtpEF.SECONDARY_RX_TS and ef_len == 16:
+                sec_scale, sec_era, _, sec_ts = struct.unpack("!BBHQ", extensions[4:16])
+                if sec_scale in (Ntp5Timescale.UTC, ):
+                    if secondary_rx_ts is None:
+                        secondary_rx_ts = {}
+                    secondary_rx_ts[sec_scale] = (sec_era, sec_ts)
             elif ef_type == NtpEF.DRAFT_ID:
                 try:
                     draft_id = extensions[4:ef_len].decode('ascii')
@@ -165,7 +179,8 @@ class NtpMessage:
         return cls(leap, version, mode, stratum, poll, precision, root_delay, root_disp,
                    receive_ts, transmit_ts, timescale, era, flags,
                    server_cookie, client_cookie, reference_id, reference_ts, origin_ts,
-                   server_info, reference_ids_req, reference_ids_resp, draft_id)
+                   server_info, reference_ids_req, reference_ids_resp, reference_ts_,
+                   secondary_rx_ts, draft_id)
 
     def get_rint(self, value):
         if self.version == 5:
@@ -202,6 +217,12 @@ class NtpMessage:
                     struct.pack("!H", self.reference_ids_req[0]) + (self.reference_ids_req[1] - 2) * " ".encode())
         if self.reference_ids_resp is not None:
             message += self.encode_ef(NtpEF.REFERENCE_IDS_RESP, self.reference_ids_resp)
+        if self.reference_ts_ is not None:
+            message += self.encode_ef(NtpEF.REFERENCE_TS, struct.pack("!Q", self.reference_ts_))
+        if self.secondary_rx_ts is not None:
+            for timescale, (era, ts) in self.secondary_rx_ts.items():
+                message += self.encode_ef(NtpEF.SECONDARY_RX_TS,
+                                          struct.pack("!BBHQ", timescale, era, 0, ts))
         if self.draft_id is not None:
             message += self.encode_ef(NtpEF.DRAFT_ID, self.draft_id.encode('ascii'))
 
@@ -256,7 +277,7 @@ class NtpClient:
 
         timescale = era = flags = server_cookie = client_cookie = None
         reference_id = reference_ts = origin_ts = None
-        server_info = reference_ids_req = draft_id = None
+        server_info = reference_ids_req = reference_ts_ = secondary_rx_ts = draft_id = None
 
         receive_ts = transmit_ts = 0
 
@@ -274,6 +295,8 @@ class NtpClient:
             server_info = 0
             reference_ids_req = (self.next_refids_fragment * (REFERENCE_IDS_OCTETS // self.refids_fragments),
                                  (REFERENCE_IDS_OCTETS // self.refids_fragments))
+            reference_ts_ = 0
+            secondary_rx_ts = {timescale: (0, 0)}
             draft_id = OUR_DRAFT_ID
         elif self.version == 4:
             reference_id = origin_ts = 0
@@ -289,6 +312,7 @@ class NtpClient:
                           receive_ts, transmit_ts, timescale, flags, era, server_cookie,
                           client_cookie, reference_id, reference_ts, origin_ts,
                           server_info=server_info, reference_ids_req=reference_ids_req,
+                          reference_ts_=reference_ts_, secondary_rx_ts=secondary_rx_ts,
                           draft_id=draft_id)
 
     def send_request(self, sock):
@@ -440,7 +464,7 @@ class NtpServer:
     def make_response(self, request, receive_ts, transmit_ts):
         timescale = era = flags = server_cookie = client_cookie = None
         reference_id = reference_ts = origin_ts = None
-        server_info = reference_ids_resp = draft_id = None
+        server_info = reference_ids_resp = reference_ts_ = secondary_rx_ts = draft_id = None
 
         root_disp = self.root_disp
         if self.stratum > 1:
@@ -464,6 +488,12 @@ class NtpServer:
             if request.reference_ids_req is not None:
                 reference_ids_resp = self.reference_ids.to_bytes(REFERENCE_IDS_OCTETS, byteorder='big') \
                         [request.reference_ids_req[0]:sum(request.reference_ids_req)]
+            if request.reference_ts_ is not None:
+                reference_ts_ = self.reference_ts
+            if request.secondary_rx_ts is not None:
+                secondary_rx_ts = {}
+                if Ntp5Timescale.UTC in request.secondary_rx_ts:
+                    secondary_rx_ts[Ntp5Timescale.UTC] = (era, receive_ts)
             if request.draft_id is not None:
                 draft_id = OUR_DRAFT_ID[:len(request.draft_id)]
 
@@ -488,6 +518,7 @@ class NtpServer:
                           self.root_delay, root_disp, receive_ts, transmit_ts, timescale, era, flags,
                           server_cookie, client_cookie, reference_id, reference_ts, origin_ts,
                           server_info=server_info, reference_ids_resp=reference_ids_resp,
+                          reference_ts_=reference_ts_, secondary_rx_ts=secondary_rx_ts,
                           draft_id=draft_id)
 
     def save_timestamps(self, receive_ts, transmit_ts):
